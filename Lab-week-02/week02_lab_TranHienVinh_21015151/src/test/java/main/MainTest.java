3b546f9aa9ec9f2eb306a807034d8bec452288aa
Tran Hien Vinh <tranvinh141203@gmail.com>
2024-10-05 04:24:01 +0700

    Tạo database

--- Lab-week-02/week02_lab_TranHienVinh_21015151/src/test/java/main/MainTest.java
+++ Lab-week-02/week02_lab_TranHienVinh_21015151/src/test/java/main/MainTest.java
@@ -19,7 +19,7 @@ import jakarta.persistence.Persistence;
  */
 public class MainTest {
     public static void main(String[] args) {
-        EntityManagerFactory emf= Persistence.createEntityManagerFactory("JPA_MSSQL");
+        EntityManagerFactory emf= Persistence.createEntityManagerFactory("mariadb");
         EntityManager em= emf.createEntityManager();
         EntityTransaction tx= em.getTransaction();
         try {
